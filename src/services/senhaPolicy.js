8d53d12d5f664b2ec50339787ea5ab17202871aa
Lucas Matheus <lukaschamposki@gmail.com>
2026-08-29 01:04:01 -0300

    fix: make password symbol check lint-safe

--- src/services/senhaPolicy.js
+++ src/services/senhaPolicy.js
@@ -1,12 +1,14 @@
 export const SENHA_MINIMA = 10;
+const SIMBOLOS = '!@#$%^&*()_+-=[]{};\'\\:"|<>?,./`~';
 
 export function erroSenhaForte(senha) {
+  const valor = senha || "";
   const faltantes = [];
-  if ((senha || "").length < SENHA_MINIMA) faltantes.push(`ao menos ${SENHA_MINIMA} caracteres`);
-  if (!/[a-z]/.test(senha || "")) faltantes.push("uma letra minúscula");
-  if (!/[A-Z]/.test(senha || "")) faltantes.push("uma letra maiúscula");
-  if (!/\d/.test(senha || "")) faltantes.push("um número");
-  if (!/[!@#$%^&*()_+\-=\[\]{};'\\:\"|<>?,./`~]/.test(senha || "")) faltantes.push("um símbolo");
+  if (valor.length < SENHA_MINIMA) faltantes.push(`ao menos ${SENHA_MINIMA} caracteres`);
+  if (!/[a-z]/.test(valor)) faltantes.push("uma letra minúscula");
+  if (!/[A-Z]/.test(valor)) faltantes.push("uma letra maiúscula");
+  if (!/\d/.test(valor)) faltantes.push("um número");
+  if (![...valor].some((caractere) => SIMBOLOS.includes(caractere))) faltantes.push("um símbolo");
 
   return faltantes.length
     ? `A senha deve conter ${faltantes.join(", ")}.`
